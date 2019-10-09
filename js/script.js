8d6d858b9eb48227932516d228153fb8fe4a78d1
KrishnaDevashish1 <krishna@enterprisebot.org>
2019-10-10 00:33:11 +0530

    Remove unnecessary code from js file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-// var gitAccountList = {};
-// var repodetails = [];
-    $("#main_search").keyup(function(){
-        var value = $('#main_search').val();
-        var repositoryURL = 'https://api.github.com/users/'+value+'/repos';
-        $.ajax({
-            type: 'GET',
-            url: repositoryURL,
-            success: function(data) {
-                createCard(data); 
-            },
-        });
+$("#main_search").keyup(function(){
+    var value = $('#main_search').val();
+    var repositoryURL = 'https://api.github.com/users/'+value+'/repos';
+    $.ajax({
+        type: 'GET',
+        url: repositoryURL,
+        success: function(data) {
+            createCard(data); 
+        },
     });
+});
 
-    function createCard(data) {
-        //console.log(data);
-        for(key in data) {
-            console.log(key);
-        }
+function createCard(data) {
+    //console.log(data);
+    for(key in data) {
+        console.log(key);
     }
+}
