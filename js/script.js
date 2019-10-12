a95784c8976a6bc42f5873e345949823b3874fec
Mrkd007 <itskd17@gmail.com>
2019-10-12 21:52:04 +0530

    Added all the basic functionality for the project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-function getUsersList(repositoryURL) {
-    var apiData;
-    $.ajax({
-        type: 'GET',
-        url: repositoryURL,
-        success: function(data) {
-            apiData = data;
-            // console.log(apiData);
-            return apiData;
-        },
-    });
-}
 var value;
 
 $("#main_search").on('input',function(){
+    $('.repo-list-container').css({'transform':'translateY(60vh)','visibility':'hidden'});
     $('.show-up-image').css('transform','translateY(0)');
     value = $('#main_search').val();
     if(value == '') {
@@ -36,6 +25,15 @@ $("#main_search").on('input',function(){
 });
 
 function createDropDown(inputElement,data) {
+    $('.repo-list-container').css({'transform':'translateY(60vh)','visibility':'hidden'});
+    $('.show-up-image').css('transform','translateY(0)');
+    if(value == '') {
+        inputHasNoData();
+        return;
+    } else {
+        inputHasData();
+    }
+    
     var listString = '';
     for(key in data.items) {
         listString += `<li class="dropdown_list__list--item" onclick="getUserRepos('${data.items[key].login}')">${data.items[key].login}</li>`;
@@ -53,6 +51,7 @@ function createDropDown(inputElement,data) {
 function getUserRepos(userName) {
     $('.dropdown_list').css({'height':'0'});
     $('.dropdown_list__list').html('');
+    $('#main_search').val(userName);
     var getrepoURL = 'https://api.github.com/users/'+userName+'/repos'
     $.ajax({
         type: 'GET',
@@ -65,7 +64,7 @@ function getUserRepos(userName) {
 
 function createCard(data) {
     $('.show-up-image').css('transform','translateY(-60vh)');
-    $('.repo-list-container').css({'visibility':'visible'});
+    $('.repo-list-container').css({'transform':'translateY(0vh)','visibility':'visible'});
     console.log(data);
     var cardString = '';
     for(key in data) {
@@ -73,9 +72,12 @@ function createCard(data) {
         cardString += `
         <div class="repo-list-card">
             <span class="title">${data[key].name}</span>
-            <span class="clone-repo">${data[key].clone_url}<i class="material-icons card -copy-icon" title="Click here to copy the clone URL" onclick="copy()">file_copy</i></span>
-            <span class="description">${data[key].description}</span>
-            <span class="last-updated">${data[key].updated_at}</span>
+            <span class="clone-repo-wrapper">
+                <span class="clone-repo" id=clone_repo_${key}>${data[key].clone_url}</span>
+                <i class="material-icons card-copy-icon" title="Click here to copy the clone URL" onclick="copy('clone_repo_${key}')">file_copy</i>
+            </span>
+            <span class="description">${checkValidDesp(data[key].description)}</span>
+            <span class="last-updated">Last Updated at : ${convertLocalDate(data[key].updated_at)}</span>
         </div>`;
     }
     $('.repo-list-card-conatiner').html(cardString);
@@ -93,5 +95,31 @@ function inputHasNoData() {
     $('.dropdown_list').css({'height':'0'});
     $('.dropdown_list__list').html('');
     
-    $('.repo-list-container').css({'visibility':'hidden'});
+    $('.repo-list-container').css({'transform':'translateY(60vh)','visibility':'hidden'});
+}
+
+function checkValidDesp(description) {
+    if(description != null) {
+        return description;
+    } else {
+        return 'No description available';
+    }
+}
+
+function convertLocalDate(oldDate) {
+    var date = new Date(oldDate);
+    date = date.toLocaleString();
+    return date;
+}
+
+function copy(element_id) {
+    var aux = document.createElement("div");
+    aux.setAttribute("contentEditable", true);
+    aux.innerHTML = document.getElementById(element_id).innerHTML;
+    aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)"); 
+    document.body.appendChild(aux);
+    aux.focus();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert('url copied');
 }
